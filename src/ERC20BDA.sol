f879b6f08844c13123ebff3da3ed87dadbb06f22
0x21q <kratochvil.kubik@gmail.com>
2025-04-27 18:32:55 +0200

    Adding additional contract functionality

--- src/ERC20BDA.sol
+++ src/ERC20BDA.sol
@@ -9,10 +9,12 @@ contract ERC20BDA is ERC20Capped, AccessControl {
     // roles
     bytes32 public constant mintingAdmin = keccak256("MINTING_ADMIN_ROLE");
     bytes32 public constant restrAdmin = keccak256("RESTR_ADMIN_ROLE");
+    bytes32 public constant idpAdmin = keccak256("IDP_ADMIN_ROLE");
 
-    uint256 public maxDailyLimit;
+    uint256 public immutable maxDailyLimit;
     uint256 public dailyMinted;
     uint256 public lastMintReset;
+    uint256 public immutable expirationTime;
 
     struct TransferLimit {
         uint256 limit;
@@ -22,34 +24,55 @@ contract ERC20BDA is ERC20Capped, AccessControl {
 
     mapping(address => TransferLimit) public transferLimits;
     mapping(address => bool) public isAddressIDP;
-    mapping(address => bool) public isAddressVerified;
+    mapping(address => uint256) public verificationTimestamp;
+    mapping(address => bool) public isAddressBlocked;
+
+    address[] public identityProviders;
+
+    event AddressVerified(address indexed account, uint256 timestamp);
+    event AddressRevoked(address indexed account);
+    event AddressBlocked(address indexed account);
+    event AddressUnblocked(address indexed account);
+    event TransferRestrictionCreated(address indexed account, uint256 limit);
+
+    // other transaction related events are already emitted in openzeppelin
 
     constructor(
         uint256 _maxSupply,
         uint256 _maxDailyLimit,
+        uint256 _expirationTimeH,
         address[] memory _mintingAdmins,
         address[] memory _restrAdmins,
-        address[] memory _identityProviders
+        address[] memory _identityProviders,
+        address[] memory _idpAdmins
     ) ERC20("ERC20BDA", "ERC") ERC20Capped(_maxSupply) {
         require(
             _maxDailyLimit <= _maxSupply,
             "Maximum daily limit cannot be larger than maximum supply."
         );
+        require(_expirationTimeH > 0, "Expiration time must be greater than 0");
 
         maxDailyLimit = _maxDailyLimit;
         lastMintReset = block.timestamp;
+        expirationTime = _expirationTimeH * 1 hours;
 
+        // initialize map for lookups and array for enumeration
         for (uint i = 0; i < _identityProviders.length; i++) {
             isAddressIDP[_identityProviders[i]] = true;
         }
+        identityProviders = _identityProviders;
 
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        // initialize roles
+        grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
 
         for (uint i = 0; i < _mintingAdmins.length; i++) {
-            _setupRole(mintingAdmin, _mintingAdmins[i]);
+            grantRole(mintingAdmin, _mintingAdmins[i]);
         }
         for (uint i = 0; i < _restrAdmins.length; i++) {
-            _setupRole(restrAdmin, _restrAdmins[i]);
+            grantRole(restrAdmin, _restrAdmins[i]);
+        }
+        for (uint i = 0; i < _idpAdmins.length; i++) {
+            grantRole(idpAdmin, _idpAdmins[i]);
         }
     }
 
@@ -67,28 +90,9 @@ contract ERC20BDA is ERC20Capped, AccessControl {
             dailyMinted + amount <= maxDailyLimit,
             "Daily mint limit exceeded"
         );
-        require(
-            isVerified[receiver],
-            "Non-verified address cannot receive tokens"
-        );
 
         dailyMinted += amount;
         _mint(receiver, amount);
-    }
-
-    function setAddressAsVerified(
-        address _address
-    ) external onlyRole(mintingAdmin) {
-        require(isAddressIDP[msg.sender], "Caller is not an IDP");
-        isVerified[_address] = true;
-        // emit AddressVerified(_address);
-    }
-
-    function setMaxDailyLimit(
-        uint256 newLimit
-    ) external onlyRole(mintingAdmin) {
-        require(newLimit <= cap(), "Exceeds max supply");
-        maxDailyLimit = newLimit;
     }
 
     function setTransferLimit(
@@ -100,6 +104,65 @@ contract ERC20BDA is ERC20Capped, AccessControl {
             used: 0,
             lastReset: block.timestamp
         });
+        emit TransferRestrictionCreated(account, limit);
+    }
+
+    function addIdentityProvider(
+        address idpAddress
+    ) external onlyRole(idpAdmin) {
+        require(!isAddressIDP[idpAddress], "Address is already an IDP");
+        identityProviders.push(idpAddress);
+        isAddressIDP[idpAddress] = true;
+    }
+
+    function removeIdentityProvider(
+        address idpAddress
+    ) external onlyRole(idpAdmin) {
+        require(isAddressIDP[idpAddress], "Address is not an IDP");
+        // swap last item with removed and pop
+        for (uint256 i = 0; i < identityProviders.length; i++) {
+            if (identityProviders[i] == idpAddress) {
+                identityProviders[i] = identityProviders[
+                    identityProviders.length - 1
+                ];
+                identityProviders.pop();
+                break;
+            }
+        }
+        isAddressIDP[idpAddress] = false;
+    }
+
+    function verifyAddressIDP(address account) external {
+        require(isAddressIDP[msg.sender], "Caller is not an IDP");
+        verificationTimestamp[account] = block.timestamp;
+        emit AddressVerified(account, block.timestamp);
+    }
+
+    function verifyAddressAdmin(address account) external onlyRole(idpAdmin) {
+        verificationTimestamp[account] = block.timestamp;
+        emit AddressVerified(account, block.timestamp);
+    }
+
+    function revokeVerification(address account) external onlyRole(idpAdmin) {
+        verificationTimestamp[account] = 0;
+        emit AddressRevoked(account);
+    }
+
+    function blockAddress(address account) external onlyRole(idpAdmin) {
+        isBlocked[account] = true;
+        emit AddressBlocked(account);
+    }
+
+    function unblockAddress(address account) external onlyRole(idpAdmin) {
+        isBlocked[account] = false;
+        emit AddressUnblocked(account);
+    }
+
+    // blocked users are still classified as verified
+    function isVerified(address account) public view returns (bool) {
+        return
+            verificationTimestamp[account] != 0 &&
+            block.timestamp <= verificationTimestamp[account] + expirationTime;
     }
 
     function _update(
@@ -107,17 +170,28 @@ contract ERC20BDA is ERC20Capped, AccessControl {
         address receiver,
         uint256 amount
     ) internal override {
-        if (receiver != address(0)) {
-            require(
-                isVerified[receiver],
-                "Non-verified address cannot receive tokens"
-            );
+        // check if sender is not blocked
+        if (sender != address(0)) {
+            require(!isBlocked[sender], "Sender is blocked");
         }
 
-        // skip limit checks for minting and burning
-        if (sender != address(0) && receiver != address(0)) {
-            TransferLimit storage limit = transferLimits[sender];
+        // check verification status
+        if (sender == address(0)) {
+            require(
+                isVerified(receiver),
+                "Cannot mint to non-verified address"
+            );
+        } else if (receiver == address(0)) {
+            require(
+                isVerified(sender),
+                "Cannot burn from non-verified address"
+            );
+        } else if (sender != address(0) && receiver != address(0)) {
+            require(isVerified(sender), "Sender is not verified");
+            require(isVerified(receiver), "Receiver is not verified");
 
+            // check transfer limit
+            TransferLimit storage limit = transferLimits[sender];
             if (block.timestamp >= limit.lastReset + 1 days) {
                 limit.used = 0;
                 limit.lastReset = block.timestamp;
@@ -128,6 +202,7 @@ contract ERC20BDA is ERC20Capped, AccessControl {
             );
             limit.used += amount;
         }
+
         super._update(sender, receiver, amount);
     }
 }
